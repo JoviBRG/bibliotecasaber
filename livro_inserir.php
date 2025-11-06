354304104576e6590549bd8867a58b4082641c94
JoviBRG <vitorbragajesus@gmail.com>
2025-11-06 08:40:01 -0300

    Livro Inserir.

--- livro_inserir.php
+++ livro_inserir.php
@@ -1,10 +1,11 @@
+80% de armazenamento usado … Se você atingir o limite, não será possível criar, editar ou fazer upload de arquivos. Aproveite 30 GB de armazenamento por R$ 4,50 R$ 1 por mês, durante 3 meses.
 <!DOCTYPE html>
 <html lang="en">
 <head>
     
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tarefa :: Inserir</title>
+    <title>Livro :: Cadastrar</title>
     <?php
     include "referencias.php";
     ?>
@@ -12,14 +13,16 @@
 <body>
     <?php
 
-    $descricao = $_POST["txtDescricao"];
-    $data_entrega = $_POST["txtData"];
-    $prioridade = $_POST["txtPrioridade"];
-    $responsavel = $_POST["txtResponsavel"];
+    $isbn = $_POST["isbn"];
+    $titulo = $_POST["titulo"];
+    $ano_publicacao = $_POST["anoPublicacao"];
+    $editora = $_POST["editora"];
+    $qtd_total = $_POST['qtdTotal'];
+    $qtd_disponivel = $_POST['qtdDisponivel'];
     
     //MONTAR COMANDO SQL
 
-    $sql = "INSERT INTO tarefa(descricao, data_entrega,prioridade,responsavel) VALUES (?,?,?,?)";
+    $sql = "INSERT INTO livro(isbn, titulo, anoPublicacao, editora, qtdTotal, qtdDisponivel) VALUES (?,?,?,?,?,?)";
 
     //PREPARAR COMANDO SQL PARA SER EXECUTADO E RELACIONAR O COMANDO QUE SERÁ EXECUTADO
 
@@ -27,12 +30,12 @@
 
     //VINCULAR OS ? COM AS VARIÁVEIS DE ENTRADA
 
-    $comando->bind_param("ssss", $descricao,$data_entrega,$prioridade,$responsavel);
+    $comando->bind_param("ssisii", $isbn,$titulo,$ano_publicacao,$editora,$qtd_total,$qtd_disponivel);
     
     //EXECUTAR COMANDO
     if ($comando->execute())
     {
-        echo "<h1>Tarefa Agendada<\h1>";
+        echo "<h1>Livro cadastrado<\h1>";
     }
     else
     {
