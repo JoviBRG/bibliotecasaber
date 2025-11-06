6475d2ae366b281711734b4db5cf211cccbeb542
JoviBRG <vitorbragajesus@gmail.com>
2025-11-06 08:47:50 -0300

    Alterado títulos em arquivos.

--- livro_editar.php
+++ livro_editar.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tarefa :: Editar</title>
+    <title>Livro :: Editar</title>
     <?php
     include "referencias.php";
     ?>
--- livro_listar.php
+++ livro_listar.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tarefas :: Listar</title>
+    <title>Livro :: Listar</title>
 
     <?php
     include "referencias.php";
